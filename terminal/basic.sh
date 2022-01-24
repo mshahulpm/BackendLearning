dce3b81c14ba2c4611cb38c519f6005af1ee8fc9
mshahulpm <mshahulpm@gmail.com>
2022-01-24 22:51:43 +0530

    terminal basic command

---
+++ terminal/basic.sh
@@ -0,0 +1,37 @@
+#############------------- BASIC TERMINAL COMMANDS -------------#############
+
+#ls                 # list all visible dir and files 
+#ls -a              # list hidden files too 
+#ls -l              # list with details like permission
+#ls -la             # hidden files too with deatails 
+#pwd                # current working dir 
+#cd                 # change dir
+#touch              # craete a file 
+#cat > hello2       # create hello2 file and after enter everythging typed willbe saved to that file until exit with ctr + c 
+#cat hello2         # print data on terminal that is inside that file 
+#cat >> hello2      # edit file 
+#mkdir afolder      # create dir 
+#clear              # clear the terminal 
+#cp hello2 new      # copy a file to another 
+#mv                 # move to another location or rename
+#rm                 # remove only file 
+#rm -r              # remove anything 
+#rmdir              # remove empty dir 
+#rmdir *            # remove all empty dir inside mentioned dir 
+#file new           # get details of a file such as type 
+#locate             # to locate somthing (will not work on that created today)
+#sudo updatedb      # to update indexdb so above cammand work on files that created today 
+#history            # history of cammands up to 1000 
+#which              # to locate a program location 
+#whatis             # give you short des about the programme 
+#apropos raed       # give all posible commands realted to read 
+#man                # get manual for a programme 
+#sudo               # super user required to run some command 
+#sudo -s            # super user mode for a while 
+#exit               # exit from super user mode 
+#su cindy           # login as super user as cindy (a user but terminal willbe in your home dir )
+#su - cindy         # the same thing above but on their home dir 
+#id                 # some data about current user like id and etc..
+#killall            # exit a process 
+#exit               # also can exit terminal 
+#users              # list all users
