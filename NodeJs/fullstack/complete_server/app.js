ff2c7cea7795022a18faac09c0552c0d68b35ff5
mshahulpm <mshahulpm@gmail.com>
2022-02-16 20:53:28 +0530

    temp finish

---
+++ NodeJs/fullstack/complete_server/app.js
@@ -0,0 +1,48 @@
+const fs = require('fs').promises
+const path = require('path')
+const express = require('express')
+
+const port = process.env.PORT || 3000
+
+const app = express()
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    next()
+})
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/error', (req, res) => {
+    throw new Error('Something went wrong')
+})
+app.get('/products', listProducts)
+
+app.use(handleError)
+app.use(notFound)
+app.listen(port, () =>
+    console.log(`Server listening on port ${port}`)
+)
+
+async function listProducts(req, res) {
+
+    console.log(req.headers.origin)
+
+    const productsFile = path.join(__dirname, 'demodata/products.json')
+    try {
+        const data = await fs.readFile(productsFile)
+        res.json(JSON.parse(data))
+    } catch (err) {
+        res.status(500).json({ error: err.message })
+    }
+}
+
+function handleError(err, req, res, next) {
+    console.error({ err })
+    if (res.headerSent) return next(err)
+    res.status(500).json({ error: 'Internal Error' })
+}
+
+function notFound(req, res) {
+    res.status(404).send('Not found')
+}
---
+++ NodeJs/fullstack/complete_server/middlewares/api.js
@@ -0,0 +1,20 @@
+const products = require('../demodata/products.json')
+
+function getAllProducts(req, res, next) {
+    res.json(products)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = {
+
+}
