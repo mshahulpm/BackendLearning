22a6850ae2711aa352b684f5ddef36ea9cd63f74
Mohammed Shahul P.M <mshahulpm@gmail.com>
2022-12-30 21:55:53 +0530

    sokks

--- orm/_mysql.js
+++ orm/_mysql.js
@@ -2,21 +2,21 @@ require('dotenv').config()
 const mysql = require('mysql2')
 const { Sequelize } = require('sequelize');
 
-// const connection = mysql.createConnection(process.env.DB_URL)
-// console.log('Connected to PlanetScale!')
-// connection.end()
+const connection = mysql.createConnection(process.env.DB_URL)
+console.log('Connected to PlanetScale!')
+connection.end()
 
 
-const sequelize = new Sequelize(process.env.DB_URL, {
-    dialect: 'mysql',
-    dialectOptions: {
-        ssl: {
-            rejectUnauthorized: true,
-        },
-    },
-    // ssl: `{rejectUnauthorized: true}`,
-});
+// const sequelize = new Sequelize(process.env.DB_URL, {
+//     dialect: 'mysql',
+//     dialectOptions: {
+//         ssl: {
+//             rejectUnauthorized: true,
+//         },
+//     },
+//     // ssl: `{rejectUnauthorized: true}`,
+// });
 
-(async () => {
-    await sequelize.authenticate()
-})()
+// (async () => {
+//     await sequelize.authenticate()
+// })()
