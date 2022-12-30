d404ed063496ad3f6f9e4eab45b6a2b99dff7759
Mohammed Shahul P.M <mshahulpm@gmail.com>
2022-12-30 21:36:04 +0530

    sequlize

---
+++ orm/_mysql.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+const mysql = require('mysql2')
+const { Sequelize } = require('sequelize');
+
+// const connection = mysql.createConnection(process.env.DB_URL)
+// console.log('Connected to PlanetScale!')
+// connection.end()
+
+
+const sequelize = new Sequelize(process.env.DB_URL, {
+    dialect: 'mysql',
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: true,
+        },
+    },
+    // ssl: `{rejectUnauthorized: true}`,
+});
+
+(async () => {
+    await sequelize.authenticate()
+})()
