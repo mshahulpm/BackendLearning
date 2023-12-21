323c31bb0ca8a02f1bb1e687489bc9a8f1e093c2
Mohammed Shahul P.M <mshahulpm@gmail.com>
2023-12-21 19:43:32 +0530

    serial datatype

--- udemy-sql/data-type/serial.js
+++ udemy-sql/data-type/serial.js
@@ -8,9 +8,22 @@ const { prisma, pgClient } = require('../db')
 
 async function main() {
 
-    await prisma.table_name.delete({
-        where: { id: 6 }
-    })
+    console.log(
+        await prisma.$queryRaw`
+    ------------- Create table for serial --------------
+    -- create table serial (
+    --     id serial not null primary key,
+    --     small_serial SMALLSERIAL not null,
+    --     big_serial BIGSERIAL not null
+    -- )
+
+    -- insert into serial default values        --- because no other columns available 
+    -- select * from serial
+
+    ------------ get serial sequence ----------------
+    -- select currval(pg_get_serial_sequence('table_name','id'))
+    `
+    );
 
 }
 
